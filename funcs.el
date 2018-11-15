b0864c37de475300f13697adcad89a249ad3a4a9
Seong Yong-ju <sei40kr@gmail.com>
2018-11-15 01:28:47 +0000

    Remove funcs.el

--- funcs.el
+++
@@ -1,9 +0,0 @@
-;;; funcs.el - ghq layer file for Spacemacs
-;;
-;; Copyright (c) 2018 Seong Yong-ju
-;;
-;; Author: Seong Yong-ju <sei40kr@gmail.com>
-;;
-;; This file is not part of GNU Emacs
-;;
-;;; License: GPLv3
